3b32ed975fd569fe8c7df9ac8abcee44e4276d20
Gortmog <9076009@gmail.com>
2016-05-13 20:48:34 +0300

    Improve OzzyZoom for mouseover, minor fixes

--- js/app.js
+++ js/app.js
@@ -80,7 +80,6 @@ var main = function () {
     //  OZZY_CLICK
     //
     
-    var timed_change;
     var current_ozzy_img = 1;
     var total_ozzy_img = 6;
     
@@ -102,12 +101,12 @@ var main = function () {
     
     function HideImg(){
         $(".ozzy img").css("opacity", "0");
-        timed_change = setTimeout(ChangeAndShowImg, 700);
+        var timed_change = setTimeout(ChangeAndShowImg, 700);
     };
     
     function ChangeAndShowImg(){
         $(".ozzy img").attr("src", "../media/ozzy_" + current_ozzy_img + ".jpg");
-        $(".ozzy img").css("opacity", "1");
+        $(".ozzy #img_main").css("opacity", "1");
     };
     
     //
@@ -117,10 +116,10 @@ var main = function () {
     var initial_height = parseInt($(".ozzy img").css("height"));
     var total_resize = 1;
     
-    $(".ozzy img").bind("mousewheel", myfunc);
+    $(".ozzy img").bind("mousewheel DOMMouseScroll", OzzyZoomScroll);
     
-    function myfunc(event){
-        var delta = event.originalEvent.wheelDelta / 120;
+    function OzzyZoomScroll(event){
+        var delta = ((event.originalEvent.wheelDelta  / 120) || -(event.detail / 3));
         var image_change = initial_height + (20 * delta);
         var resize_factor = image_change / initial_height - 1;
         total_resize = total_resize + resize_factor ;
@@ -129,11 +128,38 @@ var main = function () {
         event.preventDefault();
     };
     
-    $(".ozzy .photos").mouseleave(function(){
+    $(".ozzy .photos").on("mouseenter", OzzyZoomMouseenter);
+    
+    function OzzyZoomMouseenter(){
+        $(".ozzy #img_over").css("opacity", "1");
+        $(".ozzy #img_main").css("opacity", "0");
+        $(".ozzy .photos").on("mousemove", OzzyZoomMousemove);
+    };
+    
+    $(".ozzy .photos").on("mouseleave", OzzyZoomMouseleave);
+    
+    function OzzyZoomMouseleave(){
         $(".ozzy img").css("transform", "scale(1)");
         total_resize = 1;
-    });
+        $(".ozzy #img_over").css("opacity", "0");
+        $(".ozzy #img_main").css("opacity", "1");
+        $(".ozzy .photos").off("mousemove");
+    };
     
+    function OzzyZoomMousemove(event){
+        var ozzy_img_left = $(".ozzy #img_main").offset().left;
+        var ozzy_img_top = $(".ozzy #img_main").offset().top;
+        var ozzy_photos_left = $(".ozzy .photos").offset().left;
+        var user_x = event.clientX;
+        var user_y = event.clientY + $(window).scrollTop();
+                
+        var ozzy_over_left = - user_x + (2 * ozzy_img_left) - ozzy_photos_left;
+        var ozzy_over_top = - user_y + ozzy_img_top;
+        
+        $(".ozzy #img_over").css("left", ozzy_over_left + "px");
+        $(".ozzy #img_over").css("top", ozzy_over_top + "px");        
+    };
+        
     //
     //  IN_VIEWPORT_CHECK
     //   
