45c9acefcef5c0d6ce2286177938e3ccb2517700
Gortmog <9076009@gmail.com>
2016-06-17 17:57:17 +0300

    Fix class & spec layout, add functionality for character name input

--- js/gamers.js
+++ js/gamers.js
@@ -8,11 +8,28 @@
 
 var main = function () {
     
+    var charName;
+    
+    //
+    // START_GAME
+    //
+    
     document.querySelector(".start-game").addEventListener("click", StartGame);
 
     function StartGame() {
         document.querySelector(".start").style.display = "none";
         document.querySelector(".intro").style.display = "block";
+    }
+    
+    document.querySelector(".char-name").addEventListener("click", EnterName);
+    
+    function EnterName() {
+        if (document.querySelector("input").value !== "") {
+            charName = document.querySelector("input").value;
+            document.querySelector("input").style.display = "none";
+            document.querySelector(".char-name").style.display = "none";
+            document.querySelectorAll(".intro span")[0].innerHTML = charName;
+        }
     }
     
 };
